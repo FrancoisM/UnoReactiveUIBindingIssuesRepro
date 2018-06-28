1c561cefe6645eeb9001fac45a9290023e9f3dca
Francois <francois.mouchati@gmail.com>
2018-06-28 22:22:47 +0200

    - Moving bindings to xaml as ReactiveUI bindings in code don't seem to work with wasm
    - Visibility can now be directly bound to a bool

---
+++ UnoReactiveUIBindingIssuesRepro/UnoReactiveUIBindingIssuesRepro.Shared/InvertBoolConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace UnoReactiveUIBindingIssuesRepro.Shared
+{
+    public class InvertBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) => !(bool) value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => !(bool)value;
+    }
+}
--- UnoReactiveUIBindingIssuesRepro/UnoReactiveUIBindingIssuesRepro.Shared/MainPage.xaml.cs
+++ UnoReactiveUIBindingIssuesRepro/UnoReactiveUIBindingIssuesRepro.Shared/MainPage.xaml.cs
@@ -1,50 +1,7 @@
-﻿using System.Reactive.Disposables;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Navigation;
-using ReactiveUI;
-using UnoReactiveUIBindingIssuesRepro.ViewModels;
-
-namespace UnoReactiveUIBindingIssuesRepro
+﻿namespace UnoReactiveUIBindingIssuesRepro
 {
-    public sealed partial class MainPage : IViewFor<IMainPageViewModel>
+    public sealed partial class MainPage
     {
-        public MainPage()
-        {
-            InitializeComponent();
-            this.WhenActivated(disposables =>
-            {
-                this.OneWayBind(
-                    ViewModel,
-                    x => x.IsRefreshing,
-                    x => x.RefreshProgress.Visibility,
-                    x => x ? Visibility.Visible : Visibility.Collapsed)
-                    .DisposeWith(disposables);
-                this.OneWayBind(
-                    ViewModel,
-                    x => x.IsRefreshing,
-                    x => x.ProductsList.Visibility,
-                    x => x ? Visibility.Collapsed : Visibility.Visible)
-                    .DisposeWith(disposables);
-                this.OneWayBind(
-                        ViewModel,
-                        x => x.Products,
-                        x => x.ProductsList.ItemsSource)
-                    .DisposeWith(disposables);
-            });
-        }
-
-        object IViewFor.ViewModel
-        {
-            get => ViewModel;
-            set => ViewModel = (IMainPageViewModel)value;
-        }
-
-        public IMainPageViewModel ViewModel { get; set; }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-            ViewModel = e.Parameter as IMainPageViewModel;
-        }
+        public MainPage() => InitializeComponent();
     }
 }
